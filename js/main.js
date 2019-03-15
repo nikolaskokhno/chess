5ee665765940c3d00030eabcb01fca2a823d418b
nikolaskokhno <nikolaskokhno99@gmail.com>
2019-03-15 22:28:25 +0200

    determining the maximum minimum number

--- js/main.js
+++ js/main.js
@@ -47,7 +47,67 @@ function nextStep() {
 	document.querySelector('[posX="'+(+currentX -2) +'"][posY="'+(+currentY +1) +'"]'),
 	document.querySelector('[posX="'+(+currentX -1) +'"][posY="'+(+currentY +2) +'"]')];
 
-	console.log(vars);
+	for(let i=vars.length-1; i>=0; i--){
+		if (!vars[i] || vars[i].classList.contains('set')) {
+			vars.splice(i,1);
+		}
+	}
+
+	let nextArr = [];
+	function whatToDoNext() {
+		for(let i=0; i<vars.length; i++){
+			let nextX = vars[i].getAttribute('posX');
+			let nextY = vars[i].getAttribute('posY');
+			let nextVars = [document.querySelector('[posX="'+(+nextX +1) +'"][posY="'+(+nextY +2) +'"]'),
+				document.querySelector('[posX="'+(+nextX +2) +'"][posY="'+(+nextY +1) +'"]'),
+				document.querySelector('[posX="'+(+nextX +2) +'"][posY="'+(+nextY -1) +'"]'),
+				document.querySelector('[posX="'+(+nextX +1) +'"][posY="'+(+nextY -2) +'"]'),
+				document.querySelector('[posX="'+(+nextX -1) +'"][posY="'+(+nextY -2) +'"]'),
+				document.querySelector('[posX="'+(+nextX -2) +'"][posY="'+(+nextY -1) +'"]'),
+				document.querySelector('[posX="'+(+nextX -2) +'"][posY="'+(+nextY +1) +'"]'),
+				document.querySelector('[posX="'+(+nextX -1) +'"][posY="'+(+nextY +2) +'"]')];
+
+			for(let i=vars.length-1; i>=0; i--){
+				if (!nextVars[i] || nextVars[i].classList.contains('set')) {
+					nextVars.splice(i,1);
+				}
+			}
+
+			nextArr.push(nextVars.length);
+		}
+		return nextArr;
+	}
+
+	nextArr = whatToDoNext();
+
+	//ищем минимальное число в массиве
+	let k = nextArr.length;
+	let min = nextArr[0];
+	var index = 0;
+	while(k--){
+		if (nextArr[k] < min) {
+			min = nextArr[k];
+			index = k;
+		}
+	}
+	step++; 
+	document.querySelector('.current').classList.remove('current');
+
+	vars[index].classList.add('current');
+	vars[index].classList.add('set');
+	vars[index].innerHTML = step;
+	currentX = vars[index].getAttribute('posX');
+	currentY = vars[index].getAttribute('posY');
+
+	if (step==64) {
+		clearInterval(interval);
+		setTimeOut(() => {
+			alert('success');
+			document.location.reload(true);
+		}, 200);
+	}
 }
 
-nextStep();
+let interval = setInterval(() => {
+	nextStep();
+}, 200);

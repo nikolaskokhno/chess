bd1117fb9fd942e50ac288d16f3861502b9f366e
nikolaskokhno <nikolaskokhno99@gmail.com>
2019-03-15 20:09:21 +0200

    style chess board

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ for(let i=1; i<65; i++){
 	field.appendChild(excel);
 	excel.classList.add('excel');
 }
+
+let excel = document.getElementsByClassName('excel');
+let x=1, y=8;
+for(let i=0; i<excel.length; i++){
+	if (x>8) {
+		x=1;
+		y--;
+	}
+	excel[i].setAttribute('posX', x);
+	excel[i].setAttribute('posY', y);
+	x++;
+	if ((i%2==0 && y%2==0) || (i%2!=0 && y%2!=0)) {
+		excel[i].style.backgroundColor = "#faffaf";
+	}else{
+		excel[i].style.backgroundColor = "#6b3100";
+	}
+}
